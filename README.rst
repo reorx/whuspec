09ca00b1d78d450fb69c3c8e34d19e23b82c5791
Reorx <novoreorx@gmail.com>
2013-04-20 16:47:52 +0800

    first commit, defined models

---
+++ README.rst
@@ -0,0 +1,8 @@
+
+WHU Speciality
+==============
+
+    My graduation project.
+
+Help student improve their ability by recommending organized skill trees (called speciality) and relevant courses through this web application.
+
---
+++ whuspec/models.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import datetime
+import string
+from hashlib import md5
+from random import choice
+from torext.mongodb import Document, Struct, ObjectId
+from whuspec.database import db
+
+
+CHARS = string.letters + string.digits
+
+
+def random_string(length=10):
+    return ''.join([choice(CHARS) for i in range(length)])
+
+
+def md5_string(s):
+    if isinstance(s, unicode):
+        s = s.encode('utf8')
+    return md5(s).hexdigest()
+
+
+class Skill(Document):
+    col = db['skill']
+    __validate__ = False
+    struct = Struct({
+        'parents': [
+            {
+                'id': ObjectId,
+            }
+        ],
+        'name': str,
+        'description': str,
+        'course_id': ObjectId,
+    })
+
+
+class Speciality(Document):
+    col = db['speciality']
+    __validate__ = False
+    struct = Struct({
+        # Nearest skills
+        'skills': [
+            {
+                'id': ObjectId
+            }
+        ],
+        'level': int,
+        'name': str,
+        'description': str,
+        'creator_id': ObjectId,
+        'created_at': datetime.datetime,
+        'follower_ids': [ObjectId],
+    })
+
+
+class Course(Document):
+    col = db['course']
+    __validate__ = False
+    struct = Struct({
+        'name': str,
+        'description': str,
+    })
+
+
+class User(Document):
+    col = db['course']
+    __validate__ = False
+    struct = Struct({
+        'username': str,  # Ignore case when validating
+        'raw_username': str,
+        'password': str,
+        'password_reset_token': str,
+        'signature': str,
+        'email': str,
+        'gender': str,
+        'created_at': datetime.datetime,
+        'role': int,
+
+        # School information
+        'student_number': str,
+    })
+
+    @classmethod
+    def create(cls, email, username, password, gender, role):
+        signature = cls.generate_signature()
+        ins = cls.new(
+            username=username.lower(),
+            raw_username=username,
+            password=cls.generate_password(password, signature),
+            signature=signature,
+            email=email,
+            gender=gender,
+            created_at=datetime.datetime.now(),
+            role=role
+        )
+        ins.save()
+        return ins
+
+    def reset_password(self, new_password):
+        signature = self.generate_signature()
+        self.update_doc(
+            {
+                '$set': dict(
+                    signature=signature,
+                    password=self.generate_password(new_password, signature)
+                )
+            }
+        )
+
+    @staticmethod
+    def generate_signature():
+        return random_string(12)
+
+    @staticmethod
+    def generate_password(raw_password, signature):
+        return md5_string(signature + raw_password)
+
+    def check_password(self, raw_password):
+        return self['password'] == self.generate_password(raw_password, self['signature'])
